4b6094eab6ee4118fc4142a5dd13c4a1b57d9ac4
Phil Thomas <pthomas551@gmail.com>
2016-03-04 15:49:52 -0600

    complete hash

--- hash_set.rb
+++ hash_set.rb
@@ -1,3 +1,8 @@
+require_relative 'hash'
 class HashSet
+  def initialize
+    @set = Hashish.new
+  end
 
+  
 end
--- hash_set_spec.rb
+++ hash_set_spec.rb
@@ -2,4 +2,10 @@ require_relative 'hash_set'
 
 describe HashSet do
 
+  let(:set) { HashSet.new }
+  describe '#new' do
+    it 'instantiates a new HashSet' do
+      expect(set).to be_instance_of HashSet
+    end
+  end
 end
