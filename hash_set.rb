cd3774d8a63dff058a97e40529074cbd815ebb33
Phil Thomas <pthomas551@gmail.com>
2016-03-04 16:26:11 -0600

    refactor

--- hash_set.rb
+++ hash_set.rb
@@ -1,15 +1,21 @@
 require_relative 'hash'
 class HashSet
+
+  attr_reader :size
+
   def initialize
     @set = Hashish.new
+    @size = 0
   end
 
   def add(element)
     @set.set(element,1)
+    @size += 1
   end
 
   def remove(element)
     @set.remove(element)
+    @size -= 1
   end
 
   def contains?(element)
@@ -18,12 +24,7 @@ class HashSet
   end
 
   def iterate(&block)
-    @set.iterate {|key,value| block.call(key)}
+    @set.iterate {|key,_value| block.call(key)}
   end
 
-  def size
-    acc = 0
-    self.iterate { |v| acc += 1 if v }
-    acc
-  end
 end
